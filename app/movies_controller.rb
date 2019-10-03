5125f62be2cd46b2908e891069614299fd5baa23
Oana Berentan <oana.berentan@yahoo.com>
2019-10-03 21:15:26 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -29,8 +29,8 @@ def can_be_created_in_a_block(arg = {title: "Home Alone", release_date:1990})
   # release_date == 1990
 
   Movie.create do |m|
-    m.title = "The Room"
-    m.release_date = 2003
+  #  m.title = "The Room"
+  #  m.release_date = 2003
   end
 end
 
