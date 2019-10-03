6b7b6dfd253bf52b649bdc77a7208fb877ac9870
Oana Berentan <oana.berentan@yahoo.com>
2019-10-03 21:15:56 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -30,7 +30,7 @@ def can_be_created_in_a_block(arg = {title: "Home Alone", release_date:1990})
 
   Movie.create do |m|
      m.title = nil
-     m.release_date = 2003
+     m.release_date = nil
   end
 end
 
