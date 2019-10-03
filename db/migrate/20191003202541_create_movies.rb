6d295565cc1095791c51f78ea775048f7592597f
Oana Berentan <oana.berentan@yahoo.com>
2019-10-03 20:32:57 +0000

    Automatically backed up by Learn

--- db/migrate/20191003202541_create_movies.rb
+++ db/migrate/20191003202541_create_movies.rb
@@ -1,5 +1,5 @@
 class CreateMovies < ActiveRecord::Migration[5.2]
-  def change 
+  def change
     create_table :movies do |t|
       t.string :title
       t.integer :release_date
@@ -7,6 +7,6 @@ class CreateMovies < ActiveRecord::Migration[5.2]
       t.string :lead
       t.boolean :in_theaters
     end
-    
+
   end
 end
